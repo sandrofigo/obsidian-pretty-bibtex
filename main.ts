387ae6fdcde6795fc1f05c4266defb08f17cf9ba
Sandro Figo <sandro.figo@gmx.at>
2023-11-17 21:30:41 +0100

    Use bibtex-parser

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceItem, WorkspaceLeaf } from 'obsidian';
-import { StringHelper } from './StringHelper';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	WorkspaceItem,
+	WorkspaceLeaf
+} from 'obsidian';
+import {StringHelper} from './StringHelper';
 import './string.extensions';
+import {parse} from "@retorquere/bibtex-parser";
 
-const matcherBibTex = require('@orcid/bibtex-parse-js');
 const pluginName = "Pretty BibTeX";
 
 export default class PrettyBibTexPlugin extends Plugin {
@@ -17,33 +28,31 @@ export default class PrettyBibTexPlugin extends Plugin {
 			const codeBlock = el.createEl("div").createEl("pre").createEl("code");
 
 			try {
-				const result = matcherBibTex.toJSON(source);
-				result.forEach((it: { [x: string]: any; }) => {
-					let id = it["citationKey"];
-					let type = it["entryType"];
+				const result = parse(source);
 
-					codeBlock.createEl("span", { text: `${id}\n`, cls: "bibtex header" });
+				if (result.entries.length == 0)
+					codeBlock.createEl("span", {text: "No valid BibTex entries found!", cls: "bibtex key"});
+
+				result.entries.forEach(entry => {
+					codeBlock.createEl("span", {text: `${entry.key}\n`, cls: "bibtex header"});
+
 					if (this.settings.showType)
-						this.addKeyValueToCodeBlock(codeBlock, "Type", type);
+						this.addKeyValueToCodeBlock(codeBlock, "Type", entry.type);
 
-					let dict = it["entryTags"];
-					let keys = Object.keys(dict);
-					for (let i = 0; i < keys.length; i++) {
-						this.addKeyValueToCodeBlock(codeBlock, keys[i], dict[keys[i]]);
-					}
-
+					Object.keys(entry.fields).forEach(key => {
+						this.addKeyValueToCodeBlock(codeBlock, key, entry.fields[key].join(" and "));
+					});
 				});
 			} catch (exception) {
-				codeBlock.createEl("span", { text: "Invalid BibTeX format!", cls: "bibtex key" });
+				codeBlock.createEl("span", {text: "Invalid BibTeX format!", cls: "bibtex key"});
 			}
-			
 		});
 	}
 
 	addKeyValueToCodeBlock(codeBlock: HTMLElement, key: string, value: string): void {
-		codeBlock.createEl("span", { text: StringHelper.sanitizeKeyString(key), cls: "bibtex key" });
-		codeBlock.createEl("span", { text: ":", cls: "bibtex normal" });
-		codeBlock.createEl("span", { text: ` ${value}\n`, cls: "bibtex value" });
+		codeBlock.createEl("span", {text: StringHelper.sanitizeKeyString(key), cls: "bibtex key"});
+		codeBlock.createEl("span", {text: ":", cls: "bibtex normal"});
+		codeBlock.createEl("span", {text: ` ${value}\n`, cls: "bibtex value"});
 	}
 
 	async loadSettings() {
@@ -72,11 +81,11 @@ class SettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const { containerEl } = this;
+		const {containerEl} = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', { text: `Settings for ${pluginName}` });
+		containerEl.createEl('h2', {text: `Settings for ${pluginName}`});
 
 		new Setting(containerEl)
 			.setName('Show Type')
